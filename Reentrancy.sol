e7fa209d594a8b3f41f076c0ee588f701a1bfbc2
notabindshell <44644302+revmshell@users.noreply.github.com>
2023-03-08 00:22:11 -0600

    Update Reentrancy.sol

--- Reentrancy.sol
+++ Reentrancy.sol
@@ -15,3 +15,17 @@ contract VulnerableCOntract{
         balances[msg.sender] += msg.value;
     }
 }
+/*In this contract, users can deposit ether 
+into their account and then withdraw it later
+by calling the withdraw function. However, the 
+withdraw function is vulnerable to a reentrancy 
+attack because it updates the balance of the user 
+after sending ether to them.
+An attacker can exploit this vulnerability 
+by creating a separate contract with a fallback function 
+that calls back into the withdraw function of the 
+vulnerable contract. The attacker can then make 
+repeated calls to their contract, which will cause 
+the vulnerable contract to repeatedly send ether to 
+the attacker's contract before the initial call has 
+completed, resulting in a loss of funds.*/
