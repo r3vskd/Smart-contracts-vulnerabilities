809a49e3b1ef0c64855519e003a8552acdc1c281
notabindshell <44644302+revmshell@users.noreply.github.com>
2023-03-12 11:00:48 -0600

    Update Incorrect_calculations.sol

--- Incorrect_calculations.sol
+++ Incorrect_calculations.sol
@@ -1,8 +1,20 @@
-/*Because smart contracts deal with large amounts of money and, occasionally, various types of tokens, it’s essential that all mathematical calculations are correct. Incorrect decimal handling and fee calculations can result in the loss of funds or funds being locked indefinitely.
+/*Because smart contracts deal with large amounts of money and, occasionally,
+various types of tokens, it’s essential that all mathematical calculations
+are correct. Incorrect decimal handling and fee calculations can result in the
+loss of funds or funds being locked indefinitely.
 
-In this example, the 'balance' variable is an unsigned integer that keeps track of the amount of tokens held by the contract. The 'deposit' function allows a user to deposit tokens into the contract by adding the deposited amount to the 'balance' variable. The withdraw function allows a user to 'withdraw' tokens from the contract by subtracting the withdrawn amount and a fee (calculated as 10% of the withdrawn amount) from the 'balance' variable.
+In this example, the 'balance' variable is an unsigned integer that keeps track
+of the amount of tokens held by the contract. The 'deposit' function allows
+a user to deposit tokens into the contract by adding the deposited amount to the 'balance'
+variable. The withdraw function allows a user to 'withdraw' tokens from the contract
+by subtracting the withdrawn amount and a fee (calculated as 10% of the withdrawn amount)
+from the 'balance' variable.
 
-However, there is an error in the calculation of the fee. Since Solidity performs integer division by default, the fee will be calculated as the integer part of the result of the division, discarding any remainder. This could result in an incorrect fee amount being deducted from the 'balance' variable and the user receiving fewer tokens than expected.*/
+However, there is an error in the calculation of the fee. Since Solidity performs
+integer division by default, the fee will be calculated as the integer part of the
+result of the division, discarding any remainder. This could result in an incorrect
+fee amount being deducted from the 'balance' variable and the user receiving fewer 
+tokens than expected.*/
 
 pragma solidity ^0.8.0;
 
