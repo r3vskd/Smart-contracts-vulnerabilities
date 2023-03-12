6be58f999b35393ce611bcf7b4e4066cb8a41c94
notabindshell <44644302+revmshell@users.noreply.github.com>
2023-03-12 10:59:49 -0600

    Add files via upload

---
+++ Incorrect_calculations.sol
@@ -0,0 +1,29 @@
+/*Because smart contracts deal with large amounts of money and, occasionally, various types of tokens, it’s essential that all mathematical calculations are correct. Incorrect decimal handling and fee calculations can result in the loss of funds or funds being locked indefinitely.
+
+In this example, the 'balance' variable is an unsigned integer that keeps track of the amount of tokens held by the contract. The 'deposit' function allows a user to deposit tokens into the contract by adding the deposited amount to the 'balance' variable. The withdraw function allows a user to 'withdraw' tokens from the contract by subtracting the withdrawn amount and a fee (calculated as 10% of the withdrawn amount) from the 'balance' variable.
+
+However, there is an error in the calculation of the fee. Since Solidity performs integer division by default, the fee will be calculated as the integer part of the result of the division, discarding any remainder. This could result in an incorrect fee amount being deducted from the 'balance' variable and the user receiving fewer tokens than expected.*/
+
+pragma solidity ^0.8.0;
+
+contract IncorrectCalculationExample {
+    uint256 public balance;
+
+    function deposit(uint256 amount) public {
+        balance += amount;
+    }
+
+    function withdraw(uint256 amount) public {
+        uint256 fee = amount / 10; // Calculate fee as 10% of the withdrawal amount
+        require(balance >= amount + fee, "Insufficient balance");
+        balance -= (amount + fee);
+        // Transfer tokens to the recipient
+        // ...
+    }
+}
+
+/*To avoid this type of error, smart contract developers should use explicit decimal arithmetic or fixed-point arithmetic libraries to perform calculations that involve fractional values. In this case, they could multiply the withdrawal amount by 10 and then divide the result by 100 to calculate the fee as a decimal value, like so:*/
+
+uint256 fee = amount * 10 / 100; // Calculate fee as 10% of the withdrawal amount
+
+
