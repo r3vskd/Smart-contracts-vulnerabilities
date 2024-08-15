2cfed40be90584560672c047899348dc29b90b80
r3vskd <44644302+r3vskd@users.noreply.github.com>
2024-08-15 17:14:26 -0600

    Update Incorrect_calculations.sol

--- Incorrect_calculations.sol
+++ Incorrect_calculations.sol
@@ -37,5 +37,3 @@ contract IncorrectCalculationExample {
 /*To avoid this type of error, smart contract developers should use explicit decimal arithmetic or fixed-point arithmetic libraries to perform calculations that involve fractional values. In this case, they could multiply the withdrawal amount by 10 and then divide the result by 100 to calculate the fee as a decimal value, like so:*/
 
 uint256 fee = amount * 10 / 100; // Calculate fee as 10% of the withdrawal amount
-
-
